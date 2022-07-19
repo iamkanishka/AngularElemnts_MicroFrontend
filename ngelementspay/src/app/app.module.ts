1f0ea04b98bd37b95db3fd3604e96ced60b3fdf9
KanishkaNaik <kanishkabc123@gmail.com>
2022-07-19 23:35:45 +0530

    Created Custom Element, Deleted .angu;ar files

--- ngelementspay/src/app/app.module.ts
+++ ngelementspay/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,7 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
+import { createCustomElement } from '@angular/elements';
 
 
 
@@ -27,6 +28,15 @@ import {MatInputModule} from '@angular/material/input';
     MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+ // bootstrap: [AppComponent]
+ entryComponents:[AppComponent,DialogAnimationsExampleDialog]
 })
-export class AppModule { }
+export class AppModule { 
+constructor(private injector:Injector){}
+
+ngDoBootstrap(){
+  const element = createCustomElement(AppComponent,{injector:this.injector})
+  customElements.define('app-myPay',element)
+}
+
+}
