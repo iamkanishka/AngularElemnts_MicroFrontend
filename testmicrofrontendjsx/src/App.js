fda7bc933e161ae5d95b0280cbca5ae5e2e20897
KanishkaNaik <kanishkabc123@gmail.com>
2022-07-20 00:07:57 +0530

    created Element, Creted new project jsx and Integrated Element in the React App

---
+++ testmicrofrontendjsx/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <script src='./app-myPay.js'></script>
+     <app-mypay></app-mypay>
+
+    </div>
+  );
+}
+
+export default App;
